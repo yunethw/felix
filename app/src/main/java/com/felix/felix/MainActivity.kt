a1d3898d672ca38b27f63817cef0cada42470ee6
Yuneth Wijenayake <yunethcwij@gmail.com>
2022-12-29 13:03:10 +0530

    Rollback 9b73db0cf50cb8ca6e88a719f092492b7a9c3dcd

--- app/src/main/java/com/felix/felix/MainActivity.kt
+++ app/src/main/java/com/felix/felix/MainActivity.kt
@@ -21,7 +21,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.felix.felix.model.ServiceModel
